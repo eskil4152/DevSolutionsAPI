51cf524ab69644fdf4898e9848062f0ffbe570aa
eskil <eskiltb@hotmail.com>
2023-12-18 13:30:58 +0100

    Added functions to service which inherits from JPA. Controller uses service

--- src/main/java/com/devsolutions/DevSolutionsAPI/Controllers/ProductContoller.java
+++ src/main/java/com/devsolutions/DevSolutionsAPI/Controllers/ProductContoller.java
@@ -1,17 +1,42 @@
 package com.devsolutions.DevSolutionsAPI.Controllers;
 
 import com.devsolutions.DevSolutionsAPI.Entities.ProductEntity;
+import com.devsolutions.DevSolutionsAPI.Services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductContoller {
 
-    @GetMapping("/all")
-    public ResponseEntity<ProductEntity> getAllProducts(){
-        ProductEntity list = new ProductEntity();
+    private final ProductService productService;
 
-        return ResponseEntity.ok(list);
+    @Autowired
+    public ProductContoller(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    @GetMapping("/api/product/all")
+    public ResponseEntity<List<ProductEntity>> getAllProducts(){
+        var response = productService.getAllProducts();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/api/product/{id}")
+    public ResponseEntity<List<ProductEntity>> getProduct(@PathVariable long id) {
+        var response = productService.getProduct(id);
+
+        if (response.isPresent()) {
+            List<ProductEntity> productList = List.of(response.get());
+            return ResponseEntity.ok(productList);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/devsolutions/DevSolutionsAPI/Services/ProductService.java
+++ src/main/java/com/devsolutions/DevSolutionsAPI/Services/ProductService.java
@@ -1,7 +1,29 @@
 package com.devsolutions.DevSolutionsAPI.Services;
 
+import com.devsolutions.DevSolutionsAPI.Entities.ProductEntity;
+import com.devsolutions.DevSolutionsAPI.Repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+
+    public Optional<ProductEntity> getProduct(long id){
+        return productRepository.findById(id);
+    }
+
+    public List<ProductEntity> getAllProducts(){
+        return productRepository.findAll();
+    }
 }
