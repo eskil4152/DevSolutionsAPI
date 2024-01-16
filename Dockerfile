6977da1ff337cb6d51a352d56b47085206a7a8c0
eskil <eskiltb@hotmail.com>
2024-01-16 22:54:41 +0100

    Added csrf, docker and dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Stage 1: Build
+FROM maven:3.8.4-openjdk-17 AS builder
+WORKDIR /app
+COPY . .
+RUN mvn clean package
+
+# Stage 2: Production Stage
+FROM adoptopenjdk:17-jdk-alpine
+WORKDIR /app
+COPY --from=builder /app/target/DevSolutionsAPI-0.0.1-SNAPSHOT.jar .
+EXPOSE 8080
+CMD ["java", "-jar", "DevSolutionsAPI-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/devsolutions/DevSolutionsAPI/Security/SecurityConfig.java
+++ src/main/java/com/devsolutions/DevSolutionsAPI/Security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 @Configuration
 public class SecurityConfig {
@@ -13,13 +17,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorization -> authorization
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/moderator/**").hasAnyRole("ADMIN", "MODERATOR")
-                        .requestMatchers("/api/orders/**").authenticated()
-                        .requestMatchers("/api/**").permitAll()
-                        .anyRequest().authenticated())
-                        .csrf().disable()
-                        .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/api/moderator/**").hasAnyRole("ADMIN", "MODERATOR")
+                    .requestMatchers("/api/orders/**").authenticated()
+                    .anyRequest().permitAll())
+                    .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .csrf((csrf) -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                            .csrfTokenRequestHandler(new XorCsrfTokenRequestAttributeHandler()))
                 .build();
     }
 
