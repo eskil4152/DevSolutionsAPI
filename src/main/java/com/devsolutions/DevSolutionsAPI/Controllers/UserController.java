d35e59ea4d98fef512c087966b4f6a09ed2fd1ae
eskil <eskiltb@hotmail.com>
2024-01-17 17:24:47 +0100

    trying to fix cookies

--- src/main/java/com/devsolutions/DevSolutionsAPI/Controllers/UserController.java
+++ src/main/java/com/devsolutions/DevSolutionsAPI/Controllers/UserController.java
@@ -48,6 +48,9 @@ public class UserController {
         cookie.setMaxAge(3600);
         response.addCookie(cookie);
 
+        response.addHeader("Access-Control-Allow-Origin", "https://client-aste32wdra-nw.a.run.app");
+        response.addHeader("Access-Control-Allow-Headers", "*");
+
         System.out.println("COOKIE: " + cookie);
 
         return ResponseEntity.ok("Logged in " + username);
