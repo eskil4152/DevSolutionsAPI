e2b4c6b18530ab4fba7e21f6298d44b6fc7df47b
eskil <eskiltb@hotmail.com>
2024-01-17 19:42:13 +0100

    trying to fix cookies, added ACAO header

--- src/main/java/com/devsolutions/DevSolutionsAPI/Controllers/UserController.java
+++ src/main/java/com/devsolutions/DevSolutionsAPI/Controllers/UserController.java
@@ -48,7 +48,8 @@ public class UserController {
         cookie.setMaxAge(3600);
         response.addCookie(cookie);
 
-        response.addHeader("Access-Control-Allow-Credentials" , "true");
+        response.addHeader("Access-Control-Allow-Origin", "https://client-aste32wdra-nw.a.run.app");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
 
         System.out.println("COOKIE: " + cookie);
 
